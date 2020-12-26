715ca23c9ac4b770a0da4c4cbccf2dda6d39aa3b
jess-ang <jessica.anguiano@uabc.edu.mx>
2020-12-25 21:28:51 -0800

    Pantallas de inicio creadas

--- app/src/main/java/com/hfad/tamagotchiapp/MainActivity.java
+++ app/src/main/java/com/hfad/tamagotchiapp/MainActivity.java
@@ -1,8 +1,14 @@
 package com.hfad.tamagotchiapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,49 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Attach the SectionsPagerAdapter to the ViewPager
+        SectionsPagerAdapter pagerAdapter =
+                new SectionsPagerAdapter(getSupportFragmentManager());
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(pagerAdapter);
+
+        //Attach the ViewPager to the TabLayout
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(pager);
+    }
+
+    private class SectionsPagerAdapter extends FragmentPagerAdapter {
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+        @Override
+        public int getCount() {
+            return 3;
+        }
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return new MyPetFragment();
+                case 1:
+                    return new StatsFragment();
+                case 2:
+                    return new MyProfileFragment();
+            }
+            return null;
+        }
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                case 0:
+                    return getResources().getText(R.string.mypet_tab);
+                case 1:
+                    return getResources().getText(R.string.stats_tab);
+                case 2:
+                    return getResources().getText(R.string.profile_tab);
+            }
+            return null;
+        }
     }
 }
---
+++ app/src/main/java/com/hfad/tamagotchiapp/MyProfileFragment.java
@@ -0,0 +1,17 @@
+package com.hfad.tamagotchiapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MyProfileFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my_profile, container, false);
+    }
+}
